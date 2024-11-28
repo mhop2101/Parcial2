21d69eedc43bfb179779c2bd1a35ca0bbc10fec0
miguelMBAir <miguelagc@outlook.es>
2024-11-28 09:24:01 -0500

    +

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => ({
+        type: 'postgres',
+        host: '34.66.204.134',
+        port: 5432,
+        username: 'tecweb',
+        password: 'tecwebtecweb',
+        database: 'Parcial2',
+        entities: [],
+        synchronize: true,
+        droopSchema: true,
+        keepConnectionAlive: true,
+      }),
+    }),
+    TypeOrmModule.forFeature([]),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
