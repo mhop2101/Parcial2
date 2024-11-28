bd722c38cf5cf3d169a2be136ad2c7f21bd4197a
miguelMBAir <miguelagc@outlook.es>
2024-11-28 10:47:44 -0500

    finalll

--- src/bono/bono.module.ts
+++ src/bono/bono.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BonoService } from './bono.service';
 import { BonoController } from './bono.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Bono } from '../entities/bono.entity';
+import { Usuario } from 'src/entities/usuario.entity';
 @Module({
   controllers: [BonoController],
   providers: [BonoService],
+  imports: [
+    TypeOrmModule.forFeature([Bono, Usuario]),
+  ],
 })
-export class BonoModule {}
+export class BonoModule { }
